7d9598cc41c960654607804ebc31bc0c0b3f5527
Tomohiro Uno <uuuteenet@gmail.com>
2019-02-07 20:21:25 +0900

    Fix spec
    timesメソッドで、新しいDollarオブジェクトが返る想定

--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -3,9 +3,9 @@ require_relative '../lib/dollar'
 RSpec.describe 'Dollar' do
   it '複数回掛け算しても元の値が変わらないこと' do
     five = Dollar.new(5)
-    five.times(2)
-    expect(five.amount).to eq(10)
-    five.times(3)
-    expect(five.amount).to eq(15)
+    product = five.times(2)
+    expect(product.amount).to eq(10)
+    product = five.times(3)
+    expect(product.amount).to eq(15)
   end
 end
