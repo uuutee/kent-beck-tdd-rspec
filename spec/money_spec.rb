e225cd0f3eda09842f081733d2dd25b133ccbb9a
Tomohiro Uno <uuuteenet@gmail.com>
2019-02-14 00:55:12 +0900

    Add franc test
    Dollarの処理をコピーしてfrancのテストを作成する。
    この時点ではFrancクラスは存在しないのでredになる。
    コピペはよくないが、TDDの原則に則りすばやく失敗するテストを書き、greenにすることを目指す。

--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -11,4 +11,10 @@ RSpec.describe 'Dollar' do
     expect(Dollar.new(5).equals(Dollar.new(5))).to be_truthy
     expect(Dollar.new(5).equals(Dollar.new(6))).to be_falsy
   end
+
+  it 'franc multiplication' do
+    five = Franc.new(5)
+    expect(Franc.new(10).equals(five.times(2))).to be_truthy
+    expect(Franc.new(15).equals(five.times(3))).to be_truthy
+  end
 end
