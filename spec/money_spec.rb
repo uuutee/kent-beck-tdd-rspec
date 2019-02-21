dde803f55f76260203d4dcb451ee5adc7f6c1877
Tomohiro Uno <uuuteenet@gmail.com>
2019-02-21 10:08:06 +0900

    Use Franc factory
    Dolalr と同様、重複を排除するためにサブクラスを使わなくてすむようにしたいので、Dollar.new を Money.dollar に置き換える。

--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -13,14 +13,14 @@ RSpec.describe 'Dollar' do
   it 'equality' do
     expect(Money.dollar(5).equals(Money.dollar(5))).to be_truthy
     expect(Money.dollar(5).equals(Money.dollar(6))).to be_falsy
-    expect(Franc.new(5).equals(Franc.new(5))).to be_truthy
-    expect(Franc.new(5).equals(Franc.new(6))).to be_falsy
-    expect(Franc.new(5).equals(Money.dollar(5))).to be_falsy
+    expect(Money.franc(5).equals(Money.franc(5))).to be_truthy
+    expect(Money.franc(5).equals(Money.franc(6))).to be_falsy
+    expect(Money.franc(5).equals(Money.dollar(5))).to be_falsy
   end
 
   it 'franc multiplication' do
-    five = Franc.new(5)
-    expect(Franc.new(10).equals(five.times(2))).to be_truthy
-    expect(Franc.new(15).equals(five.times(3))).to be_truthy
+    five = Money.franc(5)
+    expect(Money.franc(10).equals(five.times(2))).to be_truthy
+    expect(Money.franc(15).equals(five.times(3))).to be_truthy
   end
 end
