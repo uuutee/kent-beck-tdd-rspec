b933f837e7675f546ff8fea638a3a87a7fbcd468
Tomohiro Uno <uuuteenet@gmail.com>
2019-03-28 11:12:43 +0900

    Implements Bank#add_rate, Bank#rate
    currency の換算レートを設定、取得できるようにメソッドの本実装を行う。
    この時点では、同じ currency 同士のPairの場合が考慮されていないためエラーになる

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,13 +1,23 @@
+require_relative 'pair'
+
 class Bank
+  attr_accessor :rates
+
+  def initialize
+    @rates = {}
+  end
+
   def reduce(source, to)
     source.reduce(self, to)
   end
 
   def add_rate(from, to, rate)
-
+    pair = Pair.new(from, to)
+    rates["#{pair.from}_#{pair.to}"] = rate
   end
 
   def rate(from, to)
-    from == 'CHF' && to == 'USD' ? 2 : 1
+    pair = Pair.new(from, to)
+    rates["#{pair.from}_#{pair.to}"]
   end
 end
