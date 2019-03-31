433d3c02038d43959d6f6a327a5093f629046447
Tomohiro Uno <uuuteenet@gmail.com>
2019-03-31 17:52:04 +0900

    Add Expression#plus interface
    Expression のインターフェースとして定義しておく。
    Sum#plus の本実装は16章内で行う。

--- lib/expression.rb
+++ lib/expression.rb
@@ -2,4 +2,8 @@ module Expression
   def reduce(bank, to)
     raise NotImplementedError
   end
+
+  def plus(addend)
+    raise NotImplementedError
+  end
 end
--- lib/sum.rb
+++ lib/sum.rb
@@ -13,4 +13,7 @@ class Sum
     amount = augend.reduce(bank, to).amount + addend.reduce(bank, to).amount
     Money.new(amount, to)
   end
+
+  def plus
+  end
 end
